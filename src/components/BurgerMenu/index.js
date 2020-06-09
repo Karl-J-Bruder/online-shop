f24af0bb10ad184271eb0311a0c2f1bab2a6c1a9
Karl Bruder <karl.j.bruder@gmail.com>
2020-06-09 11:54:56 +0900

    updated links

--- src/components/BurgerMenu/index.js
+++ src/components/BurgerMenu/index.js
@@ -1,21 +1,31 @@
-import React, { useContext } from 'react';
-import { ProductContext } from "../../contexts/ProductContext";
-import { bool } from "prop-types";
-import { StyledBurgerMenu } from "./StyledBurgerMenu";
+import React, {
+    useContext
+} from 'react';
+import {
+    ProductContext
+} from "../../contexts/ProductContext";
+import {
+    bool
+} from "prop-types";
+import {
+    StyledBurgerMenu
+} from "./StyledBurgerMenu";
 
 //For use with sake-shop project
 
-const BurgerMenu = ({ open }) => {
+const BurgerMenu = ({
+    open
+}) => {
     const cartCTX = useContext(ProductContext);
     const numItems = cartCTX.itemsCount;
     return (
         <StyledBurgerMenu open={open}>
-            <a href="/">Home</a>
-            <a href="/products">Browse Sake</a>
-            <a href="/tours">Events {"&"} Tours</a>
-            <a href="/view-cart">Cart ({numItems})</a>
-            <a href="/shipping">Shipping</a>
-            <a href="/contact">Contact us</a>
+            <a href="https://nervous-mestorf-64ded5.netlify.app/">Home</a>
+            <a href="https://nervous-mestorf-64ded5.netlify.app/products">Browse Sake</a>
+            <a href="https://nervous-mestorf-64ded5.netlify.app/tours">Events {"&"}Tours</a>
+            <a href="https://nervous-mestorf-64ded5.netlify.app/view-cart">Cart({numItems})</a>
+            <a href="https://nervous-mestorf-64ded5.netlify.app/shipping">Shipping</a>
+            <a href="https://nervous-mestorf-64ded5.netlify.app/contact" >Contact us</a>
         </StyledBurgerMenu>
     )
 }
